c5f2e1301416d3d17d9b1fbe4f1c0d83e7d87ca0
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-08-04 19:28:09 +0530

    refactor: updated code for understanding

--- Constructor/Constructor.java
+++ Constructor/Constructor.java
@@ -1,4 +1,21 @@
 package Constructor; // this allows to import the package BLEW MY FUCKING MIND
-public class Constructor {
-  
+import java.util.Arrays;
+
+class Constructor {
+  public static void main(String[] args) { // String[] args is only used to take in an input from libraries like Scanner in console applications
+    TechStack kartik = new TechStack();
+    System.out.println(kartik.acronym);
+    System.out.println(Arrays.toString(kartik.stack));
+  }
+}
+
+class TechStack{
+  String acronym;
+  String[] stack = new String[5];
+
+  TechStack(){
+    // this keyword is used to access the instance variables of a class within a constructor of that class 
+    this.acronym = "Hello There";
+    this.stack = new String[]{"React", "Node.js", "Flask", "Firebase"};
+  }
 }
