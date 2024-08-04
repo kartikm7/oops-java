27a1b9b24695db086a6ec14ccbfec4903fcb6642
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-08-04 19:32:20 +0530

    practice: extracting digits

---
+++ ConceptualTesting/ExtractDigit.java
@@ -0,0 +1,8 @@
+package ConceptualTesting;
+public class ExtractDigit {
+  public static void main(String[] args) {
+    int num = 273;
+    System.out.println(num/10);
+    System.out.println(num%10);
+  }
+}
